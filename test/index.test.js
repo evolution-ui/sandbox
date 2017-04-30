78631af32c542c002d0937cc177b28514b06452a
Chris Bracco <chris@cbracco.me>
2017-04-30 18:22:37 -0400

    test(testremoval): Remove example tests from repo

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('test environment', () => {
-  it('should work!', () => {
-    expect(true).to.be.true;
-  });
-});
--- test/utils/jsdom.js
+++
@@ -1,5 +0,0 @@
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-
-global.document = new JSDOM('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
